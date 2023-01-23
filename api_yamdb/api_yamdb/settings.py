5dd7c1394cc998729b25851eee327157ff8bec54
Стас Балджи <stack767@gmail.com>
2023-01-24 00:13:49 +0300

    1

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -63,7 +63,7 @@ WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql', # не проходят тесты
+        'ENGINE': 'django.db.backends.postgresql',  # не проходят тесты
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
